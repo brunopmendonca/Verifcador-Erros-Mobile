ce0938f19c7656ff43cbcf7e5e1b0895a0996d31
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-01-28 15:56:35 -0300

    atualização

--- src/Evtc.js
+++ src/Evtc.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {  Text, View, Button } from 'react-native';
+import {  Text, View, Button, TextInput } from 'react-native';
 
 
 const EVTC = () => ( 
-    <View style={{ flex:1 }}>
+    <View style={{  }}>
         <View style={{backgroundColor:"#ee3338", height:50,flexDirection:"row", justifyContent:'space-around', marginTop:0, padding:13, }}>
           <Text style={{ height: 50, color: "white" }}>CPTM</Text> 
           <Text style={{ height: 100, color: "white" }}>SINALIZAÇÃO</Text> 
           <Text style={{ height: 50, color: "white" }}>CBTC</Text> 
         </View>
         <View style={{backgroundColor: "black", height:5}}></View>
-        <Text style={{ height: 50, color: "black", marginTop:50, textAlign:"center", fontSize:20, }}>Digite o Binário correspondente</Text>        
+        <Text style={{ height: 50, color: "black", marginTop:50, textAlign:"center", fontSize:20, }}>Digite o binário correspondente</Text>   
+        <View style={{ alignItems:"center", justifyContent:"center", flexDirection:"row", marginTop:30}} >
+
+          <TextInput style={{width:200,height:37, borderStyle:"solid",borderRadius:2, backgroundColor:"#CDC9C9"}} placeholder="Digite somente 0 ou 1" keyboardType='numeric'/>
+          <Button 
+                    title="Pesquisar"
+                    color= "#ee3338"
+                
+          />
+      
+        </View>
+       
     </View>
       
   );
