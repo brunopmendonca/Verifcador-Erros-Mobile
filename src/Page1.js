e329d991a54fcf0a35999076f6004457768cd290
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-02-05 18:56:18 -0300

    atualização

--- src/Page1.js
+++ src/Page1.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {  Text, View, Button } from 'react-native';
+import {  Text, View, StyleSheet } from 'react-native';
+import {Button} from 'react-native-elements'
 
 const Page1 = ({navigation}) => ( 
     <View style={{ flex:1 }}>
@@ -9,100 +10,128 @@ const Page1 = ({navigation}) => (
           <Text style={{ height: 100, color: "white" }}>SINALIZAÇÃO</Text> 
           <Text style={{ height: 50, color: "white" }}>CBTC</Text> 
         </View>
-        <View style={{backgroundColor: "black", height:3}}></View>
-        <Text style={{ height: 50, color: "black", marginTop:50, textAlign:"center", fontSize:25, }}>Selecione a opção desejada</Text> 
+        <View style={{backgroundColor: "black", height:2.5}}></View>
+        <Text style={{ height: 50, color: "black", marginTop:40, textAlign:"center", fontSize:25, }}>Selecione a opção desejada</Text> 
+        
+        <View style={{flex:1, flexDirection:"row"}}>
+          <View style={{ 
+                      flex:1,
+                      margin:10,
+                    }}>
+
+                     
+
+                       <View style={{flex:1, justifyContent:"center"}}>
+                          <Button 
+                              buttonStyle = {style.botao}
+                              title="EVTC"
+                              onPress = {()=> navigation.navigate('Evtc')}
+                            />
+                       </View>
+                     
+
+                      <View style={{flex:1, justifyContent:"center"}}>
+                               <Button 
+                                buttonStyle = {style.botao}
+                                title="HNVC"
+                                onPress = {()=> navigation.navigate('Hnvc')}
+                              />
+                      </View>
+                
+
+                      <View style={{flex:1, justifyContent:"center"}}>
+
+                          <Button 
+                                buttonStyle = {style.botao}
+                                title="NCDM"
+                                onPress = {()=> navigation.navigate('Ncdm')}
+                          />
+
+                      </View>
+                          
+
+                      <View style={{flex:1, justifyContent:"center"}}>
+                          <Button 
+                              buttonStyle = {style.botao}
+                              title="VLM"
+                              onPress = {()=> navigation.navigate('Vlm')}
+                          />
+                      </View>
+                  
+
+              </View>
+
             <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,marginTop:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="EVTC"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Evtc')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="HNVC"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Hnvc')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="NCDM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Ncdm')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="VLM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Vlm')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="VLOM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Vlom')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="VPIM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Vpim')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="VROM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Vrom')}
-                  />
-            </View>
-            <View style={{ 
-                  marginRight:40, marginLeft:40,marginBottom:30,
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                }}>
-                  <Button 
-                    title="TCOM"
-                    color= "#ee3338"
-                    onPress = {()=> navigation.navigate('Tcom')}
-                  />
-            </View>
+                     flex:1,
+                     margin:10,
+                     justifyContent:"space-between"
+
+                    }}>
+              
+                      <View style={{flex:1, justifyContent:"center"}} >
+                          <Button 
+                            buttonStyle = {style.botao}
+                            title="VLOM"
+                            onPress = {()=> navigation.navigate('Vlom')}
+                          />
+                      </View>
+                
+                
+                     <View style={{flex:1, justifyContent:"center"}}>
+                        <Button 
+                            buttonStyle = {style.botao}
+                            title="VPIM"
+                            onPress = {()=> navigation.navigate('Vpim')}
+                          />
+                     </View>
+                
+                   <View style={{flex:1,justifyContent:"center"}}>
+                        <Button 
+                            buttonStyle = {style.botao}
+                            title="VROM"
+                            onPress = {()=> navigation.navigate('Vrom')}
+                        />
+                    </View>
+                
+            
+                    <View style={{flex:1,  justifyContent:"center"}}>
+                    <Button 
+                        buttonStyle = {style.botao}
+                        title="TCOM"
+                        onPress = {()=> navigation.navigate('Tcom')}
+                      />
+                    </View>
+              
+              </View>
+        </View>
              
     </View>
+
+
       
   );
+
+  const style = StyleSheet.create({
+    botao:{
+      height:"90%",
+      width: "90%",
+      alignSelf:"center",
+      backgroundColor: '#ee3338',
+      borderRadius:10,
+     
+      
+
+    },
+
+    botao2:{
+      flex:1,
+      // backgroundColor:"grey",
+      margin:10
+    }
+
+
+    
+  })
 
 
   export default Page1
